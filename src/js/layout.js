614a52810215f5569c3997372efce022f3d12186
Fernando Funez <ferfunes@gmail.com>
2019-10-12 14:16:55 +0000

    first commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
+// import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import Contacts from "./views/Contacts.js";
+import AddContact from "./views/AddContact.js";
 
-//create your first component
-export const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
-
-	return (
-		<div className="d-flex flex-column h-100">
-			<BrowserRouter>
-				<ScrollToTop>
-					<Navbar />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route path="/demo" component={Demo} />
-						<Route path="/single/:theid" component={Single} />
-						<Route render={() => <h1>Not found!</h1>} />
-					</Switch>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
-};
+export class Layout extends React.Component {
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<div>
+						<Switch>
+							<Route exact path="/index.html" component={Contacts} />
+							<Route exact path="/" component={Contacts} />
+							<Route exact path="/contacts" component={Contacts} />
+							<Route exact path="/add" component={AddContact} />
+							<Route exact path="/edit" component={AddContact} />
+							<Route render={() => <h1 className="notfound">Not found!</h1>} />
+						</Switch>
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
 
 export default injectContext(Layout);
---
+++ src/js/views/Contacts.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import ContactCard from "../component/ContactCard.js";
+import Modal from "../component/Modal";
+import { Consumer } from "../store/appContext.js";
+
+export default class Contacts extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			showModal: false
+		};
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div>
+					<p className="text-right my-3">
+						<Link className="btn btn-success" to="/add">
+							Add new contact
+						</Link>
+					</p>
+					<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+						<ul className="list-group pull-down" id="contact-list">
+							<Consumer>
+								{({ store, actions }) => {
+									console.log(store);
+									return store.contacts.map((item, index) => {
+										return (
+											<ContactCard
+												key={index}
+												name={item.full_name}
+												email={item.email}
+												phone={item.phone}
+												address={item.address}
+												onDelete={() => this.setState({ showModal: true })}
+											/>
+										);
+									});
+								}}
+							</Consumer>
+						</ul>
+					</div>
+				</div>
+				<Modal show={this.state.showModal} onClose={() => this.setState({ showModal: false })} />
+			</div>
+		);
+	}
+}
